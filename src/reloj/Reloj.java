0c844b0906dab29cf067625c287705bd20a448ac
Osvaldo <osvaldo.guerra.c008@gmail.com>
2020-02-16 11:02:50 -0600

    Reloj POO 2209

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -8,6 +8,9 @@ public class Reloj {
     private boolean stop = true;
     private int tiempo;
     
+    public Reloj(){ 
+    }
+    
     public Reloj(int hora, int minuto, int segundo, int tiempo) {
         this.hora = hora;
         this.minuto = minuto;
@@ -15,6 +18,30 @@ public class Reloj {
         this.tiempo = tiempo;
     }
 
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+    
     public void mostrarHora(){
         System.out.println(hora+":"+minuto+":"+segundo);
     }
